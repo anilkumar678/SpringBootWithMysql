729ba7f77759815c4d61c3ae95e8b5b740dda968
anilkumar678 <aniladityajava@gmail.com>
2026-02-28 10:39:46 +0530

    shoppingMart entity added

---
+++ src/main/java/com/mokshad/SpringBootWithMysql/entity/ShoppingMart.java
@@ -0,0 +1,7 @@
+package com.mokshad.SpringBootWithMysql.entity;
+
+public class ShoppingMart {
+    private int shoppingMartId;
+    private String shoppingMartName;
+    private String shoppingMartLocation;
+}
